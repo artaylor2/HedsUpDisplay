c0bbd5ae5ac1dfa8942d5d7d79ec06d6c8166972
Alix Taylor <artaylor2@NIC.EDU>
2021-02-25 16:21:45 -0800

    Updates made to 0-2.py

--- tutorialCode/0-2.py
+++ tutorialCode/0-2.py
@@ -1,3 +1,12 @@
 import PySimpleGUI as sg
 
-event, values = sg.Window('Get filename example', [[sg.Text('Filename')], [sg.Input(), sg.FileBrowse()],[sg.OK(), sg.Cancel()] ]).read(close=True)
+sg.theme('Dark Blue 3')
+
+layout = [  [sg.Text('Filename')], 
+            [sg.Input(), sg.FileBrowse()],
+            [sg.OK(), sg.Cancel()]  ]
+
+window = sg.Window('Get filename example', layout)
+
+event, values = window.read()
+window.close()
